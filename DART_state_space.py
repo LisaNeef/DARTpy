aa5a0db007c1b25d4ab75253f0fd75212dd5160f
Lisa <lneef@geomar.de>
2015-08-31 11:28:09 +0200

    make linewidth an input parameter to plot_diagnostic_profile

--- DART_state_space.py
+++ DART_state_space.py
@@ -1839,7 +1839,7 @@ def DART_diagn_to_array(E,hostname='taurus',debug=False):
 
 	return Vmatrix,lat,lon,lev,new_daterange
 
-def plot_diagnostic_profiles(E=dart.basic_experiment_dict(),Ediff=None,color="#000000",linestyle='-',hostname='taurus',log_levels=True,debug=False):
+def plot_diagnostic_profiles(E=dart.basic_experiment_dict(),Ediff=None,color="#000000",linestyle='-',linewidth = 2,hostname='taurus',log_levels=True,debug=False):
 
 	"""
 	Plot a vertical profile of some DART diagnostic / variable, 
@@ -1923,7 +1923,7 @@ def plot_diagnostic_profiles(E=dart.basic_experiment_dict(),Ediff=None,color="#0
 		M = M1
 
         # plot the profile 
-        cs = plt.plot(M,lev,color=color,linestyle=linestyle)
+        cs = plt.plot(M,lev,color=color,linestyle=linestyle,linewidth=linewidth)
 
         plt.ylabel('Level (hPa)')
 	if log_levels:
