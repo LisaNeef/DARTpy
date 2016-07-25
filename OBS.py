ec4f3dcd9967bd9206e6688c149e01590827a633
Lisa <lneef@geomar.de>
2016-07-25 16:17:49 +0200

    adding (so far still empty) module for codes related to observations

---
+++ OBS.py
@@ -0,0 +1,7 @@
+# THis module contaiins codes for retrieving and visualizing various observation types. 
+
+# load the required packages  
+import numpy as np
+import datetime
+import os.path
+from netCDF4 import Dataset
