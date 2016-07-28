09d04c5808f098c2dd8ea76732d8ad6b9c1660b1
Lisa <lneef@geomar.de>
2016-07-28 14:16:42 +0200

    completed first-draft subroutines to return high res radiosonde data as a pandas dataframe

--- OBS.py
+++ OBS.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd  
 import datetime
 import experiment_settings as es
+import DART as dart
 #import os.path
 #from netCDF4 import Dataset
 
@@ -27,6 +28,8 @@ def HRRS_as_DF(OBS,hostname='taurus'):
 	# first read in station information as a dataframe 
 	stationdata = HRRS_station_data(hostname)
 	
+	# initialize an empy list which will hold the data frames for each station and time 
+	DFlist=[]
 
 	# because the HRRS data are sorted by years, loop over the years in the daterange
 	DR=OBS['daterange']
@@ -43,18 +46,66 @@ def HRRS_as_DF(OBS,hostname='taurus'):
 				if stationdata.loc[int(s)]['Lat'] >= OBS['latrange'][0] 
 				and stationdata.loc[int(s)]['Lat'] <= OBS['latrange'][1] ]
 
-		print(stations_lat)
-
 		# trim list down to the ones that fit into the longitude range 
 		stations_latlon = [s for s in stations_lat
 				if stationdata.loc[int(s)]['Lon'] >= OBS['lonrange'][0] 
 				and stationdata.loc[int(s)]['Lon'] <= OBS['lonrange'][1] ]
 
 		# also compute the subset of the requested daterange that fits into this year. 
+		year_daterange =  dart.daterange(date_start=datetime.datetime(YYYY,1,1,0,0,0), periods=365*4, DT='6H')
+		DR2 = set(year_daterange).intersection(DR)
+		
+		# also find the dir where the station data live 
+		datadir = es.obs_data_paths('HRRS',hostname)
 
 		# now loop over available stations, and for each one, retrieve the data 
 		# that fit into the requested daterange 
 		for s in stations_latlon:	
+
+			# loop over dates, and retrieve data if available 
+			for dd in DR2:
+				datestr = dd.strftime("%Y%m%d%H")
+				ff = datadir+'/'+str(YYYY)+'/'+str(s)+'/'+str(s)+'-'+datestr+'.dat'
+				if os.path.exists(ff):
+					# read in the station data 
+					D = read_HRRS_data(ff)
+		
+					# also add a column holding the date 
+					D['Date'] = pd.Series(dd, index=D.index)
+				
+					# get rid of some unneeded columns 
+					weird_number=list(D.columns.values)[1]
+					useless_cols=['Time','Dewpt','RH','Ucmp','Vcmp','spd','dir', 'Wcmp',  'Ele', 'Azi', 'Qp', 'Qt', 'Qrh', 'Qu', 'Qv', 'QdZ',weird_number]
+					D.drop(useless_cols,inplace=True,axis=1)
+
+
+					# append to list of data frames 
+					DFlist.append(D)
+
+
+	# merge the list of data frames into a single DF using list comprehension 
+	DFout = pd.concat(DFlist, axis=0)
+	#DF = pd.merge(DF,DF2,how='outer')
+
+	return(DFout)
+
+def read_HRRS_data(ff):
+
+	"""
+	Read in a .dat file from SPARC high-res radiosonde data 
+	Input ff is a string pointing to the full path of the desired file. 
+	"""
+
+	D= pd.read_csv(ff,skiprows=13,error_bad_lines=False,delim_whitespace=True)
+	colnames=list(D.columns.values)
+	obscode=list(D.columns.values)[0]
+	D.rename(columns={obscode:'Obs.Code'}, inplace=True)
+	D['Station Number'] = pd.Series(obscode[0:5], index=D.index)
+
+	# kick out the first two rows - they hold units and symbols 
+	D.drop(D.index[[0,1]], inplace=True)
+
+	return(D)
 
 def HRRS_stations_available_per_year(YYYY):
 
